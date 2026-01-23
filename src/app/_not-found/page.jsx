05451a11a13d4cc18d4b570dba7c094a1c21c62d
Renato Castillo <castillopayat@gmail.com>
2026-01-24 02:25:54 +0800

    rename notfound

---
+++ src/app/_not-found/page.jsx
@@ -0,0 +1,12 @@
+
+
+
+export default function NotFoundPage() {
+  return (
+    <div style={{ textAlign: "center", padding: "5rem" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, we couldn’t find the page you were looking for.</p>
+    </div>
+  );
+}
+
