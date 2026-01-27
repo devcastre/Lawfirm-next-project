891b98bfae576cf072a3b3c9bce37eb746507186
Renato Castillo <castillopayat@gmail.com>
2026-01-28 04:00:13 +0800

    return nav and footer

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,7 +3,7 @@
 
 export default function NotFoundPage() {
   return (
-    <div style={{ textAlign: "center", padding: "5rem" }}>
+    <div style={{ textAlign: "center", padding: "5rem", color: "white" }}>
       <h1>404 - Page Not Found</h1>
       <p>Sorry, we couldn’t find the page you were looking for.</p>
     </div>
